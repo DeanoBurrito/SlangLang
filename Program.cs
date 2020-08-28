1491b8e2b76f4761168f6181ee86886855bb786b
Dean Tuckey <dean243@hotmail.com>
2020-08-28 11:26:42 +1000

    Repo Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlangLang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
