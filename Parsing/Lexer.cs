582d6ab3ace5f769fc05f0a06be138e2cb49ce41
Dean Tuckey <dean243@hotmail.com>
2020-08-31 14:56:16 +1000

    Moved majority of code in LexNext() into switch statement. Replaced usage of MoveNext() with simple increment.

--- Parsing/Lexer.cs
+++ Parsing/Lexer.cs
@@ -34,145 +34,145 @@ namespace SlangLang.Parsing
         }
 
         private LanguageToken LexNext()
-        {
-            if (currChar >= sourceStore.GetLength())
-                return new LanguageToken(LanguageTokenType.EndOfFile, "", new TextLocation(filename, 0, 0));
-            
-            char current = sourceStore.GetCharAt(currChar);
+        {   
+            char current = PeekNext(0);
             int start = currChar;
             TextLocation location = sourceStore.GetLocation(start);
             location.filename = filename;
             location.length = 1;
 
-            if (char.IsWhiteSpace(current))
-            {
-                //whitespace
-                while (char.IsWhiteSpace(current))
-                {
-                    current = MoveNext();
-                }
-                location.length = currChar - start;
-                return new LanguageToken(LanguageTokenType.Whitespace, sourceStore.GetSubstring(start, currChar - start), location);
-            }
-            if (char.IsDigit(current))
-            {
-                //some kind of number
-                while (char.IsDigit(current))
-                {
-                    current = MoveNext();
-                }
-                location.length = currChar - start;
-                return new LanguageToken(LanguageTokenType.IntegerNumber, sourceStore.GetSubstring(start, currChar - start), location);
-            }
-
-            if (current == '"')
-            {
-                //start of a string literal
-                current = MoveNext();
-                while (current != '"')
-                {
-                    current = MoveNext();
-                    if (current == '\0')
-                    {
-                        diagnostics.AddFailure("Lexer", "Expected \" to end string literal, found end of file.", location, DateTime.Now);
-                        return new LanguageToken(LanguageTokenType.EndOfFile, "", location);
-                    }
-                }
-                MoveNext();
-                location.length = currChar - start;
-                return new LanguageToken(LanguageTokenType.String, sourceStore.GetSubstring(start, currChar - start), location);
-            }
-
-            if (char.IsLetter(current))
-            {
-                //text
-                while (char.IsLetter(current))
-                {
-                    current = MoveNext();
-                }
-
-                location.length = currChar - start;
-                string text = sourceStore.GetSubstring(start, currChar - start);
-                LanguageTokenType keyword = LanguageFacts.GetKeyword(text);
-                return new LanguageToken(keyword, text, location);
-            }
-
             switch (current)
             {
+                case '\0':
+                    currChar++;
+                    return new LanguageToken(LanguageTokenType.EndOfFile, "", new TextLocation(filename, 0, 0));
                 case '+':
-                    MoveNext();
+                    currChar++;
                     return new LanguageToken(LanguageTokenType.Plus, "+", location);
                 case '-':
-                    MoveNext();
+                    currChar++;
                     return new LanguageToken(LanguageTokenType.Minus, "-", location);
                 case '*':
-                    MoveNext();
+                    currChar++;
                     return new LanguageToken(LanguageTokenType.Star, "*", location);
                 case '/':
-                    MoveNext();
+                    currChar++;
                     return new LanguageToken(LanguageTokenType.ForwardSlash, "/", location);
                 case ';':
-                    MoveNext();
+                    currChar++;
                     return new LanguageToken(LanguageTokenType.Semicolon, ";", location);
                 case '(':
-                    MoveNext();
+                    currChar++;
                     return new LanguageToken(LanguageTokenType.OpenParanthesis, "(", location);
                 case ')':
-                    MoveNext();
+                    currChar++;
                     return new LanguageToken(LanguageTokenType.CloseParathesis, "(", location);
                 case '!':
                     if (PeekNext(1) == '=')
                     {
-                        MoveNext(); MoveNext();
+                        currChar += 2;
                         location.length = 2;
                         return new LanguageToken(LanguageTokenType.ExclamationEquals, "!=", location);
                     }
                     else
                     {
-                        MoveNext();
+                        currChar++;
                         return new LanguageToken(LanguageTokenType.Exclamation, "!", location);
                     }
                 case '=':
                     if (PeekNext(1) == '=')
                     {
-                        MoveNext(); MoveNext();
+                        currChar += 2;
                         location.length = 2;
                         return new LanguageToken(LanguageTokenType.EqualsEquals, "==", location);
                     }
                     else
                     {
-                        MoveNext();
+                        currChar++;
                         return new LanguageToken(LanguageTokenType.Equals, "=", location);
                     }
                 case '&':
                     if (PeekNext(1) == '&')
                     {
-                        MoveNext(); MoveNext();
+                        currChar += 2;
                         location.length = 2;
                         return new LanguageToken(LanguageTokenType.AndAnd, "&&", location);
                     }
                     else
                     {
-                        MoveNext();
+                        currChar++;
                         return new LanguageToken(LanguageTokenType.And, "&", location);
                     }
                 case '|':
                     if (PeekNext(1) == '|')
                     {
-                        MoveNext(); MoveNext();
+                        currChar += 2;
                         location.length = 2;
                         return new LanguageToken(LanguageTokenType.PipePipe, "||", location);
                     }
                     else
                     {
-                        MoveNext();
+                        currChar++;
                         return new LanguageToken(LanguageTokenType.Pipe, "|", location);
                     }
-            }
+                default:
+                {
+                    if (char.IsLetter(current))
+                    {
+                        //text
+                        while (char.IsLetter(current))
+                        {
+                            current = MoveNext();
+                        }
 
-            diagnostics.AddFailure("Lexer", "Invalid character in input file '" + current + "'", location, DateTime.Now);
-            currChar++;
-            return new LanguageToken(LanguageTokenType.BadToken, "", location);
+                        location.length = currChar - start;
+                        string text = sourceStore.GetSubstring(start, currChar - start);
+                        LanguageTokenType keyword = LanguageFacts.GetKeyword(text);
+                        return new LanguageToken(keyword, text, location);
+                    }
+                    else if (char.IsWhiteSpace(current))
+                    {
+                        //whitespace
+                        while (char.IsWhiteSpace(current))
+                        {
+                            current = MoveNext();
+                        }
+                        location.length = currChar - start;
+                        return new LanguageToken(LanguageTokenType.Whitespace, sourceStore.GetSubstring(start, currChar - start), location);
+                    }
+                    else if (char.IsDigit(current))
+                    {
+                        //some kind of number
+                        while (char.IsDigit(current))
+                        {
+                            current = MoveNext();
+                        }
+                        location.length = currChar - start;
+                        return new LanguageToken(LanguageTokenType.IntegerNumber, sourceStore.GetSubstring(start, currChar - start), location);
+                    }
+                    else if (current == '"')
+                    {
+                        //start of a string literal
+                        current = MoveNext();
+                        while (current != '"')
+                        {
+                            current = MoveNext();
+                            if (current == '\0')
+                            {
+                                diagnostics.AddFailure("Lexer", "Expected \" to end string literal, found end of file.", location, DateTime.Now);
+                                return new LanguageToken(LanguageTokenType.EndOfFile, "", location);
+                            }
+                        }
+                        currChar++;
+                        location.length = currChar - start;
+                        return new LanguageToken(LanguageTokenType.String, sourceStore.GetSubstring(start, currChar - start), location);
+                    }
+
+                    diagnostics.AddFailure("Lexer", "Invalid character in input file '" + current + "'", location, DateTime.Now);
+                    currChar++;
+                    return new LanguageToken(LanguageTokenType.BadToken, "", location);
+                }
+            } 
         }
 
         private char PeekNext(int offset)
