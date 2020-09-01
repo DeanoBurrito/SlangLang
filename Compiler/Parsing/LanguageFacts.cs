fb03d2c6b5beaa51f41d172fc7d5a343a4b1d0b0
Dean Tuckey <dean243@hotmail.com>
2020-09-01 20:13:40 +1000

    Added test coverage to languagefacts.

--- Compiler/Parsing/LanguageFacts.cs
+++ Compiler/Parsing/LanguageFacts.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace SlangLang.Parsing
 {
-    internal static class LanguageFacts
+    public static class LanguageFacts
     {
         public static LanguageTokenType GetKeyword(string text)
         {
@@ -53,6 +53,52 @@ namespace SlangLang.Parsing
                     return 5;
                 default:
                     return 0;
+            }
+        }
+
+        public static string GetText(LanguageTokenType token)
+        {
+            switch (token)
+            {
+                case LanguageTokenType.And:
+                    return "&";
+                case LanguageTokenType.AndAnd:
+                    return "&&";
+                case LanguageTokenType.Pipe:
+                    return "|";
+                case LanguageTokenType.PipePipe:
+                    return "||";
+                case LanguageTokenType.Equals:
+                    return "=";
+                case LanguageTokenType.EqualsEquals:
+                    return "==";
+                case LanguageTokenType.Exclamation:
+                    return "!";
+                case LanguageTokenType.ExclamationEquals:
+                    return "!=";
+
+                case LanguageTokenType.KeywordFalse:
+                    return "false";
+                case LanguageTokenType.KeywordTrue:
+                    return "true";
+                
+                case LanguageTokenType.Plus:
+                    return "+";
+                case LanguageTokenType.Minus:
+                    return "-";
+                case LanguageTokenType.Star:
+                    return "*";
+                case LanguageTokenType.ForwardSlash:
+                    return "/";
+
+                case LanguageTokenType.OpenParanthesis:
+                    return "(";
+                case LanguageTokenType.CloseParathesis:
+                    return ")";
+                case LanguageTokenType.Semicolon:
+                    return ";";
+                default:
+                    return null;
             }
         }
     }
---
+++ SlangLang.Tests/LanguageFactsTests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using SlangLang.Parsing;
+
+namespace SlangLang.Tests
+{
+    public class LanguageFactsTests
+    {
+        [Theory]
+        [MemberData(nameof(GetRoundtripData))]
+        public void GetTextRoundtrip(LanguageTokenType tokenType)
+        {
+            string text = LanguageFacts.GetText(tokenType);
+            if (text == null)
+                return;
+            
+            LanguageToken[] tokens = new Lexer(new Debug.Diagnostics(DateTime.Now), text, "Tests").LexAll();
+            Assert.Equal(tokens.Last().tokenType, LanguageTokenType.EndOfFile);
+            Array.Resize(ref tokens, tokens.Length - 1);
+
+            LanguageToken token = Assert.Single(tokens);
+            Assert.Equal(token.tokenType, tokenType);
+            Assert.Equal(token.text, text);
+        }
+
+        public static IEnumerable<object[]> GetRoundtripData()
+        {
+            LanguageTokenType[] types = (LanguageTokenType[])Enum.GetValues(typeof(LanguageTokenType));
+            foreach (LanguageTokenType l in types)
+                yield return new object[] { l };
+        }
+    }
+}
