1b604024e2c01c1c26a8cf7b245954b4e63d770a
Dean Tuckey <dean243@hotmail.com>
2020-09-01 21:41:15 +1000

    Changed class names slightly to accurately represent them more.

---
+++ SlangLang.Tests/AssertingExpressionTree.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using SlangLang.Parsing;
+
+namespace SlangLang.Tests
+{
+    internal sealed class AssertingExpressionTree : IDisposable
+    {
+        private readonly IEnumerator<ExpressionNode> enumerator;
+        
+        public AssertingExpressionTree(ExpressionNode node)
+        {
+            enumerator = Flattern(node).GetEnumerator();
+        }
+
+        private static IEnumerable<ExpressionNode> Flattern(ExpressionNode node)
+        {
+            Stack<ExpressionNode> stack = new Stack<ExpressionNode>();
+            stack.Push(node);
+
+            while (stack.Count > 0)
+            {
+                ExpressionNode popped = stack.Pop();
+                yield return popped;
+
+                List<ExpressionNode> children = popped.GetChildren();
+                children.Reverse();
+                foreach (ExpressionNode child in children)
+                {
+                    stack.Push(child);
+                }
+            }
+        }
+
+        public void AssertNodeAndToken(ExpressionNodeType nodeType, LanguageTokenType type, string text)
+        {
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(nodeType, enumerator.Current.nodeType);
+            
+            LanguageToken token = Assert.IsType<LanguageToken>(enumerator.Current.token);
+            Assert.Equal(type, token.tokenType);
+            Assert.Equal(text, token.text);
+        }
+
+        public void AssertNode(ExpressionNodeType type)
+        {
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(type, enumerator.Current.nodeType);
+        }
+
+        public void Dispose()
+        {
+            Assert.False(enumerator.MoveNext());
+            enumerator.Dispose();
+        }
+    }
+}
