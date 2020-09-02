f4c293d2125cd719019003d6853bac4868f710bb
Dean Tuckey <dean243@hotmail.com>
2020-09-02 21:28:04 +1000

    Evaluator cleanup.

--- SlangLang/Evaluation/Evaluator.cs
+++ SlangLang/Evaluation/Evaluator.cs
@@ -26,88 +26,108 @@ namespace SlangLang.Evaluation
 
         private object EvaluateExpression(BoundExpression node)
         {   
-            if (node is BoundLiteralExpression lit)
+            switch (node.nodeType)
             {
-                return lit.value;
-            }
-            if (node is BoundVariableExpression var)
-            {
-                return variables[var.variable];
-            }
-            if (node is BoundAssignmentExpression assign) //'ass' haha almost.
-            {
-                object value = EvaluateExpression(assign.expression);
-                variables[assign.variable] = value;
-                return value;
-            }
-            if (node is BoundUnaryExpression unary)
-            {
-                object operandResult = EvaluateExpression(unary.operand);
-                Type operandType = operandResult.GetType();
-                if (operandType == typeof(int))
-                {
-                    int resultInt = (int)operandResult;
-                    switch (unary.op.unaryOperator)
-                    {
-                        case BoundUnaryOperatorType.Negate:
-                            return -resultInt;
-                    }
-                }
-                else if (operandType == typeof(bool))
-                {
-                    bool resultBool = (bool)operandResult;
-                    switch (unary.op.unaryOperator)
-                    {
-                        case BoundUnaryOperatorType.Not:
-                            return !resultBool;
-                    }
-                }
-
-                diagnostics.EvaluatorError_UnexpectedUnaryOperator(unary.op, unary.textLocation.start);
-            }
-            if (node is BoundBinaryExpression bin)
-            {
-                object leftResult = EvaluateExpression(bin.left);
-                object rightResult = EvaluateExpression(bin.right);
-                Type leftType = leftResult.GetType();
-                Type rightType = rightResult.GetType();
-                if (leftType == typeof(int) && rightType == typeof(int))
-                {
-                    int leftInt = (int)leftResult;
-                    int rightInt = (int)rightResult;
-                    switch (bin.op.binaryOperator)
-                    {
-                        case BoundBinaryOperatorType.Addition:
-                            return leftInt + rightInt;
-                        case BoundBinaryOperatorType.Subtract:
-                            return leftInt - rightInt;
-                        case BoundBinaryOperatorType.Multiplication:
-                            return leftInt * rightInt;
-                        case BoundBinaryOperatorType.Division:
-                            return leftInt / rightInt;
-                    }
-                }
-                else if (leftType == typeof(bool) && rightType == typeof(bool))
-                {
-                    bool leftBool = (bool)leftResult;
-                    bool rightBool = (bool)rightResult;
-                    switch (bin.op.binaryOperator)
-                    {
-                        case BoundBinaryOperatorType.ConditionalOr:
-                            return leftBool || rightBool;
-                        case BoundBinaryOperatorType.ConditionalAnd:
-                            return leftBool && rightBool;
-                    }
-                }
-                if (bin.op.binaryOperator == BoundBinaryOperatorType.Equals)
-                    return Equals(leftResult, rightResult);
-                else if (bin.op.binaryOperator == BoundBinaryOperatorType.NotEquals)
-                    return !Equals(leftResult, rightResult);
-                
-                diagnostics.EvaluatorError_UnxpectedBinaryOperator(bin.op, bin.textLocation.start);
+                case BoundNodeType.LiteralExpression:
+                    return EvaluateLiteralExpression((BoundLiteralExpression)node);
+                case BoundNodeType.VariableExpression:
+                    return EvaluateVariableExpression((BoundVariableExpression)node);
+                case BoundNodeType.AssignmentExpression:
+                    return EvaluateAssignmentExpression((BoundAssignmentExpression)node);
+                case BoundNodeType.UnaryExpression:
+                    return EvaluateUnaryExpression((BoundUnaryExpression)node);
+                case BoundNodeType.BinaryExpression:
+                    return EvaluateBinaryExpression((BoundBinaryExpression)node);
             }
 
             throw new Exception("Unexpected node in evaluator.");
+        }
+
+        private object EvaluateLiteralExpression(BoundLiteralExpression expr)
+        {
+            return expr.value;
+        }
+
+        private object EvaluateVariableExpression(BoundVariableExpression expr)
+        {
+            return variables[expr.variable];
+        }
+
+        private object EvaluateAssignmentExpression(BoundAssignmentExpression expr)
+        {
+            object value = EvaluateExpression(expr.expression);
+            variables[expr.variable] = value;
+            return value;
+        }
+
+        private object EvaluateUnaryExpression(BoundUnaryExpression expr)
+        {
+            object operandResult = EvaluateExpression(expr.operand);
+            Type operandType = operandResult.GetType();
+            if (operandType == typeof(int))
+            {
+                int resultInt = (int)operandResult;
+                switch (expr.op.unaryOperator)
+                {
+                    case BoundUnaryOperatorType.Negate:
+                        return -resultInt;
+                }
+            }
+            else if (operandType == typeof(bool))
+            {
+                bool resultBool = (bool)operandResult;
+                switch (expr.op.unaryOperator)
+                {
+                    case BoundUnaryOperatorType.Not:
+                        return !resultBool;
+                }
+            }
+
+            diagnostics.EvaluatorError_UnexpectedUnaryOperator(expr.op, expr.textLocation.start);
+            throw new Exception("Unexpected unary operator in evaluator!");
+        }
+
+        private object EvaluateBinaryExpression(BoundBinaryExpression expr)
+        {
+            object leftResult = EvaluateExpression(expr.left);
+            object rightResult = EvaluateExpression(expr.right);
+            Type leftType = leftResult.GetType();
+            Type rightType = rightResult.GetType();
+            if (leftType == typeof(int) && rightType == typeof(int))
+            {
+                int leftInt = (int)leftResult;
+                int rightInt = (int)rightResult;
+                switch (expr.op.binaryOperator)
+                {
+                    case BoundBinaryOperatorType.Addition:
+                        return leftInt + rightInt;
+                    case BoundBinaryOperatorType.Subtract:
+                        return leftInt - rightInt;
+                    case BoundBinaryOperatorType.Multiplication:
+                        return leftInt * rightInt;
+                    case BoundBinaryOperatorType.Division:
+                        return leftInt / rightInt;
+                }
+            }
+            else if (leftType == typeof(bool) && rightType == typeof(bool))
+            {
+                bool leftBool = (bool)leftResult;
+                bool rightBool = (bool)rightResult;
+                switch (expr.op.binaryOperator)
+                {
+                    case BoundBinaryOperatorType.ConditionalOr:
+                        return leftBool || rightBool;
+                    case BoundBinaryOperatorType.ConditionalAnd:
+                        return leftBool && rightBool;
+                }
+            }
+            if (expr.op.binaryOperator == BoundBinaryOperatorType.Equals)
+                return Equals(leftResult, rightResult);
+            else if (expr.op.binaryOperator == BoundBinaryOperatorType.NotEquals)
+                return !Equals(leftResult, rightResult);
+            
+            diagnostics.EvaluatorError_UnxpectedBinaryOperator(expr.op, expr.textLocation.start);
+            throw new Exception("Unexpected binary operator in evaluator!");
         }
     }
 }
